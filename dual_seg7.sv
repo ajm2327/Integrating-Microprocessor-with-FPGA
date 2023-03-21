92c8b3e49fd198463ad810869824d1b55e673357
ajm2327 <72291395+ajm2327@users.noreply.github.com>
2023-03-21 15:01:38 -0700

    Update dual_seg7.sv

--- dual_seg7.sv
+++ dual_seg7.sv
@@ -12,8 +12,9 @@ module dual_seg7(input logic blank,
 						input logic [7:0] data,
 						output logic [6:0] segments1,
 						output logic [6:0] segments2);
-						
+		//Left display takes switches 7:4 data for "16's" place				
 		seg7  leftdisp (blank, test, data[7:4], segments1);
+		//Right display takes switches 3:0 data for one's place
 		seg7 rightdisp (blank, test, data[3:0], segments2);
 		
 endmodule
