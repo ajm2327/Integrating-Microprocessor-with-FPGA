8eed96814cfa1fe71c5d810fdb783a3183049d09
ajm2327 <72291395+ajm2327@users.noreply.github.com>
2023-03-21 15:12:35 -0700

    Update pc_tb.sv

--- pc_tb.sv
+++ pc_tb.sv
@@ -10,10 +10,10 @@ module pc_tb(input logic [9:0] SW,
 					input logic [1:0] KEY,
 					output logic [6:0] HEX0, HEX1);
 					
-		logic[7:0] PC; //internal PC i think
-		
+	logic[7:0] PC; //internal PC variable
+		//Call pc module
 		pc pc1(.clk(KEY[0]), .reset(~KEY[1]), .LOAD_PC(SW[9]), .INCR_PC(SW[8]), .ADDR(SW[7:0]), .PC(PC));
-		
+		//Output PC to right pair of 7seg displays
 		dual_seg7 dualright(.blank(0), .test(0), .data(PC), .segments1(HEX1), .segments2(HEX0));
 		
 		endmodule
