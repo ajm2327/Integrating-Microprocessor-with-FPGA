2bcac8fcf78a9b59d6ed6d8457d63e1a617dc496
ajm2327 <72291395+ajm2327@users.noreply.github.com>
2023-03-21 15:11:30 -0700

    Update pc.sv

--- pc.sv
+++ pc.sv
@@ -2,7 +2,7 @@
 //Assignment: lab4
 //Author: Adam Montano
 //Module Name: pc
-//Module Function: (poppycock) program counter
+//Module Function: program counter
 //**************************************************************************************
 
 
@@ -10,7 +10,7 @@ module pc( input logic clk, reset, LOAD_PC, INCR_PC,
 				input logic [7:0] ADDR,
 				output logic [7:0] PC);
 				
-// da instruction fetch cycle
+//instruction fetch cycle
 	always_ff @(posedge clk, posedge reset)
 		
 		if (reset) PC <= 0; // clear PC when reset
